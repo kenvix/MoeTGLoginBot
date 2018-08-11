10086391807ffdcf9b4dd312f2d9d812a1c1953b
Kenvix <kenvixzure@live.com>
2018-08-11 16:45:58 +0800

    90%

--- MoecraftBot/app.ts
+++ MoecraftBot/app.ts
@@ -1,12 +1,14 @@
 ﻿import fs = require("fs");
 import os = require("os");
 import util = require('util');
-import Telegraf from "telegraf"
+import Telegraf, { ContextMessageUpdate } from "telegraf";
 import process = require("process");
 import path = require("path");
 import superagent = require("superagent");
 import querystring = require('querystring');
 import commander = require("commander");
+import { IncomingMessage } from "telegraf/typings/telegram-types";
+import Telegram = require('telegraf/telegram');
 
 commander.version('1.0.0')
     .option('-x, --debug', 'Enable debug functions')
@@ -34,6 +36,13 @@ enum UserStatus {
     Done
 }
 
+enum ChatType {
+    Private = "private",
+    Group = "group",
+    SuperGroup = "supergroup",
+    Channel = "channel"
+}
+
 interface IAuthData {
     "email": string
     "password": string
@@ -47,27 +56,57 @@ interface IAuthSession {
     };
 }
 
-const bot = new Telegraf(cfg.tg.key);
+let Agent = null;
+
+switch (cfg.tg.proxy.type) {
+    case "socks5":
+        let SocksAgent = require('socks5-https-client/lib/Agent');
+        Agent = new SocksAgent({
+            socksHost: cfg.tg.proxy.socks5.host,
+            socksPort: cfg.tg.proxy.socks5.port,
+            socksUsername: cfg.tg.proxy.socks5.user,
+            socksPassword: cfg.tg.proxy.socks5.password,
+        });
+        break;
+
+    case "https":
+        const HttpsProxyAgent = require('https-proxy-agent')
+        Agent = new HttpsProxyAgent(cfg.tg.proxy.https.url);
+        break;
+}
+
+const bot = new Telegraf(cfg.tg.key, {
+    telegram: {           // Telegram options
+        agent: Agent        // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
+    }
+});
+const telegram = new Telegram(cfg.tg.key, {
+    agent: Agent
+});
 let AuthSession: IAuthSession = {};
 
 bot.command('start', (ctx) => {
-    ctx.reply("欢迎使用 MoeCraft Telegram 认证管理工具\n请输入您的 MoeCraft 用户中心的用户名或邮箱")
-    AuthSession[ctx.chat.id] = {
-        "name": ctx.chat.username,
-        "status": UserStatus.WaitingUsername, 
-        "data": {
-            "email": "",
-            "password": ""
+    if (ctx.chat.type == ChatType.Private) {
+        ctx.reply("欢迎使用 MoeCraft Telegram 认证管理工具\n请输入您的 MoeCraft 用户中心的用户名或邮箱")
+        AuthSession[ctx.message.from.id] = {
+            "name": ctx.message.from.username,
+            "status": UserStatus.WaitingUsername,
+            "data": {
+                "email": "",
+                "password": ""
+            }
         }
     }
 });
 
 bot.command('session', (ctx) => {
-    ctx.reply("Session ID: " + ctx.chat.id + "\n" + "User Name: " + ctx.chat.username);
-    if (typeof (ctx.chat.id) == "undefined" || typeof (AuthSession[ctx.chat.id]) == "undefined" || AuthSession[ctx.chat.id].status == UserStatus.Idle)
-        ctx.reply("Session not started or marked as idle");
-    else
-        ctx.reply("Session status: " + AuthSession[ctx.chat.id].status.toString());
+    ctx.reply("User ID: " + ctx.message.from.id + "\nChat ID: " + ctx.chat.id + "\n" + "User Name: " + ctx.message.from.username);
+    if (ctx.chat.type == ChatType.Private) {
+        if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle)
+            ctx.reply("Session not started or marked as idle");
+        else
+            ctx.reply("Session status: " + AuthSession[ctx.message.from.id].status.toString());
+    }
 });
 
 bot.command('dump', (ctx) => {
@@ -75,76 +114,116 @@ bot.command('dump', (ctx) => {
 });
 
 bot.command('cancel', (ctx) => {
-    AuthSession[ctx.chat.id] = undefined;
-    ctx.reply("会话已结束，若要重新开始认证，请输入 /start");
+    if (ctx.chat.type == ChatType.Private) {
+        AuthSession[ctx.message.from.id] = undefined;
+        ctx.reply("会话已结束，若要重新开始认证，请输入 /start");
+    }
 });
 
 bot.command('back', (ctx) => {
-    if (typeof (ctx.chat.id) == "undefined" || typeof (AuthSession[ctx.chat.id]) == "undefined" || AuthSession[ctx.chat.id].status == UserStatus.Idle) {
-        ctx.reply("会话尚未开始，不能回退");
-        return;
+    if (ctx.chat.type == ChatType.Private) {
+        if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle) {
+            ctx.reply("会话尚未开始，不能回退");
+            return;
+        }
+        switch (AuthSession[ctx.message.from.id].status) {
+            case UserStatus.WaitingPassword:
+                AuthSession[ctx.message.from.id].status = UserStatus.WaitingUsername;
+                ctx.reply("已回退到上一步：请输入您的 MoeCraft 用户中心的用户名或邮箱");
+                break;
+            case UserStatus.WaitingUsername:
+                AuthSession[ctx.message.from.id] = undefined;
+                ctx.reply("已回退到顶层：会话已结束，若要重新开始认证，请输入 /start");
+                break;
+        }
     }
-    switch (AuthSession[ctx.chat.id].status) {
-        case UserStatus.WaitingPassword:
-            AuthSession[ctx.chat.id].status = UserStatus.WaitingUsername;
-            ctx.reply("已回退到上一步：请输入您的 MoeCraft 用户中心的用户名或邮箱");
-            break;
-        case UserStatus.WaitingUsername:
-            AuthSession[ctx.chat.id] = undefined;
-            ctx.reply("已回退到顶层：会话已结束，若要重新开始认证，请输入 /start");
-            break;
+});
+
+bot.on('edited_message', (ctx) => {
+    if (ctx.chat.type == ChatType.Private) {
+        let editMessage = ctx.editedMessage;
+        if (typeof (editMessage.from.id) == "undefined" || typeof (AuthSession[editMessage.from.id]) == "undefined" || AuthSession[editMessage.from.id].status == UserStatus.Idle) return;
+        switch (AuthSession[editMessage.from.id].status) {
+            case UserStatus.WaitingPassword:
+                AuthSession[editMessage.from.id].data.email = editMessage.text;
+                ctx.reply("编辑用户名成功，接下来，请输入您的密码：");
+                break;
+            case UserStatus.Done:
+                ctx.reply("您已完成认证，直接点击之前给出的链接即可加入群组。若要重新认证，请输入 /start");
+                break;
+        }
     }
+});
+
+bot.on('new_chat_members', (ctx) => {
+    ctx.message.new_chat_members.forEach((value, key) => {
+        if (value.is_bot) return;
+        if (typeof (AuthSession[value.id]) == "undefined" || AuthSession[ctx.message.from.id].status != UserStatus.Done) {
+            try {
+                
+            } catch {}
+        }
+    });
 });
 
 bot.on('message', (ctx) => {
-    if (typeof (ctx.chat.id) == "undefined" || typeof (AuthSession[ctx.chat.id]) == "undefined" || AuthSession[ctx.chat.id].status == UserStatus.Idle)
-        return;
-    let session = AuthSession[ctx.chat.id];
-    let msg = ctx.message.text;
-    switch (AuthSession[ctx.chat.id].status) {
-        case UserStatus.WaitingUsername:
-            session.data.email = msg;
-            session.status = UserStatus.WaitingPassword;
-            ctx.reply("接下来，请输入您的密码：\n若输入错误，请输入 /back 回退到上一步");
-            break;
-        case UserStatus.WaitingPassword:
-            session.data.password = msg;
-            ctx.reply("请稍等，正在进行认证");
-            try {
-                superagent
-                    .post(cfg.api.url)
-                    .send(querystring.stringify({
-                        login: session.data.email,
-                        password: session.data.password
-                    }))
-                    .set('Useragent', 'MoeCraft Bot')
-                    .end((err, res) => {
-                        if (err) throw err;
-                        if (typeof (res) == "undefined" || res.text.length < 1)
-                            throw new Error("接收认证服务器返回数据失败");
-                        let data = JSON.parse(res.text);
-                        if (typeof (res) == "undefined" || !res)
-                            throw new Error("解析认证服务器返回数据失败");
-                        if (data.status != 0) {
-                            ctx.reply("错误：" + data.info);
-                            return;
-                        }
-                        if (typeof (data.uid) != "undefined" && data.uid > 0) {
-                            AuthSession[ctx.chat.id].status = UserStatus.Done;
-                            ctx.reply("认证成功: 欢迎回来，" + data.name + "\n感谢您加入 MoeCraft，以下是 MoeCraft Group 邀请链接：\n" + cfg.api.group + "\n本次会话结束后链接失效。为了确保安全，请在入群后删除本次会话");
-                            return;
-                        }
-                        throw new Error("未知错误");
-                    });
-            } catch (ex) {
-                ctx.reply("认证出错：" + ex.name + ":" + ex.message);
-                console.log("<!> Failed to authorize user: " + session.name);
-                console.log(ex);
-            }
-            break;
+    if (ctx.chat.type == ChatType.Private) {
+        if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle)
+            return;
+        let session = AuthSession[ctx.message.from.id];
+        let msg = ctx.message.text;
+        switch (AuthSession[ctx.message.from.id].status) {
+            case UserStatus.WaitingUsername:
+                session.data.email = msg;
+                session.status = UserStatus.WaitingPassword;
+                ctx.reply("接下来，请输入您的密码：\n若输入错误，直接您发送的编辑那条消息即可");
+                break;
+            case UserStatus.WaitingPassword:
+                DoAuth(ctx, ctx.message, session);
+                break;
+        }
     }
 });
 
+function DoAuth(ctx: ContextMessageUpdate, ctxmsg: IncomingMessage, session) {
+    session.data.password = ctxmsg.text;
+    ctx.reply("请稍等，正在进行认证");
+    try {
+        superagent
+            .post(cfg.api.url)
+            .send(querystring.stringify({
+                login: session.data.email,
+                password: session.data.password
+            }))
+            .set('Useragent', 'MoeCraft Bot')
+            .end((err, res) => {
+                if (err) throw err;
+                if (typeof (res) == "undefined" || res.text.length < 1)
+                    throw new Error("接收认证服务器返回数据失败");
+                let data = JSON.parse(res.text);
+                if (typeof (res) == "undefined" || !res)
+                    throw new Error("解析认证服务器返回数据失败");
+                if (data.status != 0) {
+                    ctx.reply("错误：" + data.info);
+                    return;
+                }
+                if (typeof (data.uid) != "undefined" && data.uid > 0) {
+                    AuthSession[ctxmsg.from.id].status = UserStatus.Done;
+                    ctx.reply("认证成功: 欢迎回来，" + data.name + "\n感谢您加入 MoeCraft，以下是 MoeCraft Group 邀请链接：\n" + cfg.api.group + "\n本次会话结束后链接失效。为了确保安全，请在入群后删除本次会话");
+                    return;
+                }
+                throw new Error("未知错误");
+            });
+    } catch (ex) {
+        ctx.reply("认证出错：" + ex.name + ":" + ex.message);
+        console.log("<!> Failed to authorize user: " + session.name);
+        console.log(ex);
+    }
+}
 
-console.log("Start polling telegram message");
+if (Agent == null)
+    console.log("Start polling telegram message (no proxy)");
+else
+    console.log("Start polling telegram message. Proxy type: " + cfg.tg.proxy.type);
+
 bot.startPolling();
