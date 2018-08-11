a543b6c03a1f4f3be4b4e98c10b478e0c159be8d
Balthild <ibalthild@gmail.com>
2018-08-12 00:21:51 +0800

    Re-sturcture

--- MoecraftBot/app.ts
+++ MoecraftBot/app.ts
@@ -1,16 +1,51 @@
-﻿import fs = require("fs");
-import os = require("os");
-import util = require('util');
-import Telegraf, { ContextMessageUpdate } from "telegraf";
-import process = require("process");
-import path = require("path");
-import superagent = require("superagent");
-import querystring = require('querystring');
-import commander = require("commander");
-import { IncomingMessage, ChatMember } from "telegraf/typings/telegram-types";
-import Telegram = require('telegraf/telegram');
-import { fail } from "assert";
-import { exists } from "fs";
+﻿import fse from "fs-extra";
+import Telegraf, { ContextMessageUpdate, Telegram } from "telegraf";
+import process from "process";
+import path from "path";
+import superagent from "superagent";
+import querystring from "querystring";
+import commander from "commander";
+import { IncomingMessage } from "telegraf/typings/telegram-types";
+
+enum UserStatus {
+    Idle,
+    WaitingUsername,
+    WaitingPassword,
+    Done
+}
+
+enum ChatType {
+    Private = "private",
+    Group = "group",
+    SuperGroup = "supergroup",
+    Channel = "channel"
+}
+
+interface IAuthData {
+    email: string;
+    password: string;
+    uid?: number;
+    name?: string;
+}
+
+interface IAuthSession {
+    name: string; // Telegram Name
+    status: UserStatus;
+    data: IAuthData;
+}
+
+interface IAuthSessions {
+    [index: number]: IAuthSession;
+}
+
+interface IGroup { // Telegram Chat ID
+    enabled: boolean;
+    operator: number; // Operator Telegram ID
+}
+
+interface IGroups {
+    [index: number]: IGroup;
+}
 
 commander.version('1.0.0')
     .option('-x, --debug', 'Enable debug functions')
@@ -19,7 +54,7 @@ commander.version('1.0.0')
 
 console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
 
-(async () => {
+async function main() {
     const dir = path.normalize(__dirname + "/");
     const cfg = require('config.json')(dir + 'config.json');
 
@@ -33,49 +68,15 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
         return commander.debug;
     }
 
-    async function isAdmin(members: Promise<ChatMember[]>, id: number): Promise<boolean> {
-        let MembersArray = await members;
-        for (var i = 0; i < MembersArray.length; i++) {
-            if (MembersArray[i].user.id == id) return true;
+    async function isAdmin(ctx: ContextMessageUpdate, id: number): Promise<boolean> {
+        let admins = await ctx.getChatAdministrators();
+
+        for (const admin of admins) {
+            if (admin.user.id == id)
+                return true;
         }
+
         return false;
-    }
-
-    enum UserStatus {
-        Idle,
-        WaitingUsername,
-        WaitingPassword,
-        Done
-    }
-
-    enum ChatType {
-        Private = "private",
-        Group = "group",
-        SuperGroup = "supergroup",
-        Channel = "channel"
-    }
-
-    interface IAuthData {
-        "email": string
-        "password": string
-        "uid"?: number
-        "name"?: string
-    }
-
-    interface IAuthSession {
-        [index: number]: { //Telegram UID
-            "name": string //Telegram Name
-            "status": UserStatus,
-            "data": IAuthData
-        };
-    }
-
-
-    interface IGroups {
-        [index: number]: { //Telegram Chat ID
-            "enabled": boolean
-            "operator": number //Operator Telegram ID
-        };
     }
 
     let Agent = null;
@@ -106,65 +107,80 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
     const telegram = new Telegram(cfg.tg.key, {
         agent: Agent
     });
-    let AuthSession: IAuthSession = {};
-    let Groups: IGroups = {}
 
-    function SaveGroups() {
-        fs.writeFile(dir + 'groups.json', JSON.stringify(Groups), (err) => {
-            if (err) throw err;
-        });
+    let AuthSession: IAuthSessions = {};
+    let Groups: IGroups = {};
+
+    async function SaveGroups() {
+        await fse.writeFile(dir + 'groups.json', JSON.stringify(Groups));
     }
 
     bot.command('start', async (ctx) => {
-        if (ctx.chat.type == ChatType.Private) {
-            ctx.reply("欢迎使用 MoeCraft Telegram 认证管理工具\n请输入您的 MoeCraft 用户中心的用户名或邮箱")
-            AuthSession[ctx.message.from.id] = {
-                "name": ctx.message.from.username,
-                "status": UserStatus.WaitingUsername,
-                "data": {
-                    "email": "",
-                    "password": ""
-                }
+        if (ctx.chat!.type !== ChatType.Private)
+            return;
+
+        ctx.reply("欢迎使用 MoeCraft Telegram 认证管理工具\n请输入您的 MoeCraft 用户中心的用户名或邮箱");
+
+        AuthSession[ctx.message!.from!.id!] = {
+            name: ctx.message!.from!.username!,
+            status: UserStatus.WaitingUsername,
+            data: {
+                email: "",
+                password: ""
             }
         }
     });
 
     bot.command('session', async (ctx) => {
-        ctx.reply("User ID: " + ctx.message.from.id + "\nChat ID: " + ctx.chat.id + "\n" + "User Name: " + ctx.message.from.username);
-        if (ctx.chat.type == ChatType.Private) {
-            if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle)
+        const user = ctx.message && ctx.message.from;
+        const userId = user && user.id;
+        const userName = user && user.username;
+
+        const chat = ctx.chat;
+
+        ctx.reply(`User ID: ${userId}\nChat ID: ${chat && chat.id!}\nUsername: ${userName}`);
+
+        if (chat && chat.type == ChatType.Private) {
+            if (!userId || AuthSession[userId] || AuthSession[userId].status == UserStatus.Idle)
                 ctx.reply("Session not started or marked as idle");
             else
-                ctx.reply("Session status: " + AuthSession[ctx.message.from.id].status.toString());
+                ctx.reply("Session status: " + AuthSession[userId].status.toString());
         }
     });
 
     bot.command('dump', async (ctx) => {
-        if (!isDebugMode()) return;
+        if (!isDebugMode())
+            return;
+
         ctx.reply("AuthSession:\n" + JSON.stringify(AuthSession));
         ctx.reply("Groups:\n" + JSON.stringify(Groups));
     });
 
     bot.command('cancel', async (ctx) => {
-        if (ctx.chat.type == ChatType.Private) {
-            AuthSession[ctx.message.from.id] = undefined;
+        if (ctx.chat && ctx.chat.type == ChatType.Private) {
+            delete AuthSession[ctx.message!.from!.id!];
             ctx.reply("会话已结束，若要重新开始认证，请输入 /start");
         }
     });
 
     bot.command('back', async (ctx) => {
-        if (ctx.chat.type == ChatType.Private) {
-            if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle) {
+        const user = ctx.message && ctx.message.from;
+        const userId = user && user.id;
+        const userName = user && user.username;
+
+        if (ctx.chat && ctx.chat.type == ChatType.Private) {
+            if (!userId || !AuthSession[userId] || AuthSession[userId].status == UserStatus.Idle) {
                 ctx.reply("会话尚未开始，不能回退");
                 return;
             }
-            switch (AuthSession[ctx.message.from.id].status) {
+
+            switch (AuthSession[userId].status) {
                 case UserStatus.WaitingPassword:
-                    AuthSession[ctx.message.from.id].status = UserStatus.WaitingUsername;
+                    AuthSession[userId].status = UserStatus.WaitingUsername;
                     ctx.reply("已回退到上一步：请输入您的 MoeCraft 用户中心的用户名或邮箱");
                     break;
                 case UserStatus.WaitingUsername:
-                    AuthSession[ctx.message.from.id] = undefined;
+                    delete AuthSession[userId];
                     ctx.reply("已回退到顶层：会话已结束，若要重新开始认证，请输入 /start");
                     break;
             }
@@ -172,29 +188,31 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
     });
 
     bot.command('help', async (ctx) => {
-        let helpmsg = "通用MoeCraft认证管理工具 (MoeCraft Bot)"
-            + "\n" + "作者：Kenvix [ https://kenvix.com ]"
-            + "\n" + "========================"
-            + "\n" + "/status  查看 MoeCraft 认证管理 状态"
-            + "\n" + "/enable  在本群组启用 MoeCraft 认证管理"
-            + "\n" + "/disable 在本群组停用 MoeCraft 认证管理"
-            + "\n" + "/help    查看帮助"
-            + "\n" + "/start   开始认证会话(仅限私聊)"
-            + "\n" + "/back    回退认证会话(仅限私聊)"
-            + "\n" + "/cancel  终止认证会话(仅限私聊)"
-            + "\n" + "/session 输出会话信息"
-        ctx.reply(helpmsg);
+        ctx.reply(`通用 MoeCraft 认证管理工具 (MoeCraft Bot)
+作者：Kenvix [ https://kenvix.com ]
+========================
+/status  查看 MoeCraft 认证管理 状态
+/enable  在本群组启用 MoeCraft 认证管理
+/disable 在本群组停用 MoeCraft 认证管理
+/help    查看帮助
+/start   开始认证会话(仅限私聊)
+/back    回退认证会话(仅限私聊)
+/cancel  终止认证会话(仅限私聊)
+/session 输出会话信息`);
     });
     
     bot.command('enable', async (ctx) => {
         try {
-            let ChatAdmins = ctx.getChatAdministrators();
-            if (ctx.chat.all_members_are_administrators || await isAdmin(ChatAdmins, ctx.from.id)) {
+            if (!ctx.chat || !ctx.from)
+                throw new Error("未知错误");
+
+            if (ctx.chat.all_members_are_administrators || await isAdmin(ctx, ctx.from.id)) {
                 Groups[ctx.chat.id] = {
                     "enabled": true,
                     "operator": ctx.from.id
-                }
+                };
                 SaveGroups();
+
                 ctx.reply("已对群组 " + ctx.chat.id + " 启用 MoeCraftBot");
             } else {
                 throw new Error("操作者必须是管理员");
@@ -206,13 +224,16 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
 
     bot.command('disable', async (ctx) => {
         try {
-            let ChatAdmins = ctx.getChatAdministrators();
-            if (ctx.chat.all_members_are_administrators || isAdmin(ChatAdmins, ctx.from.id)) {
+            if (!ctx.chat || !ctx.from)
+                throw new Error("未知错误");
+
+            if (ctx.chat.all_members_are_administrators || isAdmin(ctx, ctx.from.id)) {
                 Groups[ctx.chat.id] = {
-                    "enabled": false,
-                    "operator": ctx.from.id
-                }
+                    enabled: false,
+                    operator: ctx.from.id
+                };
                 SaveGroups();
+
                 ctx.reply("已对群组 " + ctx.chat.id + " 禁用 MoeCraftBot");
             } else {
                 throw new Error("操作者必须是管理员");
@@ -223,65 +244,85 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
     });
 
     bot.command('status', async (ctx) => {
-        if (typeof (Groups[ctx.chat.id]) != "undefined" && Groups[ctx.chat.id].enabled)
-            ctx.reply("+ 已对群组 " + ctx.chat.id + " 启用 MoeCraftBot");
+        const chatId = ctx.chat && ctx.chat.id;
+        if (chatId && Groups[chatId].enabled)
+            ctx.reply("+ 已对群组 " + chatId + " 启用 MoeCraftBot");
         else
-            ctx.reply("- 未对群组 " + ctx.chat.id + " 启用 MoeCraftBot");
+            ctx.reply("- 未对群组 " + chatId + " 启用 MoeCraftBot");
     });
 
     bot.on('edited_message', async (ctx) => {
-        if (ctx.chat.type == ChatType.Private) {
-            let editMessage = ctx.editedMessage;
-            if (typeof (editMessage.from.id) == "undefined" || typeof (AuthSession[editMessage.from.id]) == "undefined" || AuthSession[editMessage.from.id].status == UserStatus.Idle) return;
-            switch (AuthSession[editMessage.from.id].status) {
-                case UserStatus.WaitingPassword:
-                    AuthSession[editMessage.from.id].data.email = editMessage.text;
-                    ctx.reply("编辑用户名成功，接下来，请输入您的密码：");
-                    break;
-                case UserStatus.Done:
-                    
-                    ctx.reply("您已完成认证，直接点击之前给出的链接即可加入群组。若要重新认证，请输入 /start");
-                    break;
-            }
+        if (!ctx.chat || ctx.chat.type !== ChatType.Private)
+            return;
+
+        const user = ctx.editedMessage && ctx.editedMessage.from;
+        const userId = user && user.id;
+
+        if (!userId || !AuthSession[userId] || AuthSession[userId].status == UserStatus.Idle)
+            return;
+
+        switch (AuthSession[userId].status) {
+            case UserStatus.WaitingPassword:
+                AuthSession[userId].data.email = ctx.editedMessage!.text!;
+                ctx.reply("编辑用户名成功，接下来，请输入您的密码：");
+                break;
+            case UserStatus.Done:
+                ctx.reply("您已完成认证，直接点击之前给出的链接即可加入群组。若要重新认证，请输入 /start");
+                break;
         }
     });
 
     bot.on('new_chat_members', async (ctx) => {
-        if (typeof (Groups[ctx.chat.id]) == "undefined" || Groups[ctx.chat.id].enabled) {
-            ctx.message.new_chat_members.forEach((value, key) => {
-                if (!commander.bots && value.is_bot) return;
-                if (typeof (AuthSession[value.id]) == "undefined" || AuthSession[value.id].status != UserStatus.Done) {
-                    telegram.kickChatMember(ctx.chat.id, value.id)
-                        .catch((error) => { ctx.reply("踢出未认证用户失败：" + error.message); });
-                } else {
-                    ctx.reply("欢迎 " + AuthSession[value.id].data.name + " [ " + value.first_name + " ] 加入 MoeCraft Group!");
-                }
-            });
-        }
+        const chatId = ctx.chat!.id;
+        if (Groups[chatId] && !Groups[chatId].enabled)
+            return;
+
+        ctx.message!.new_chat_members!.forEach((member, key) => {
+            if (!commander.bots && member.is_bot)
+                return;
+
+            if (!AuthSession[member.id] || AuthSession[member.id].status != UserStatus.Done) {
+                // Typing for telegraf has a bug
+                (telegram as any).kickChatMember(chatId, member.id)
+                    .catch((error: Error) => {
+                        ctx.reply("踢出未认证用户失败：" + error.message);
+                    });
+            } else {
+                ctx.reply(`欢迎 ${AuthSession[member.id].data.name} [ ${member.first_name} ] 加入 MoeCraft Group!`);
+            }
+        });
     });
 
     bot.on('message', async (ctx) => {
-        if (ctx.chat.type == ChatType.Private) {
-            if (typeof (ctx.message.from.id) == "undefined" || typeof (AuthSession[ctx.message.from.id]) == "undefined" || AuthSession[ctx.message.from.id].status == UserStatus.Idle)
-                return;
-            let session = AuthSession[ctx.message.from.id];
-            let msg = ctx.message.text;
-            switch (AuthSession[ctx.message.from.id].status) {
-                case UserStatus.WaitingUsername:
-                    session.data.email = msg;
-                    session.status = UserStatus.WaitingPassword;
-                    ctx.reply("接下来，请输入您的密码：\n若刚才的用户名输入错误，直接您发送的编辑那条消息即可");
-                    break;
-                case UserStatus.WaitingPassword:
-                    DoAuth(ctx, ctx.message, session);
-                    break;
-            }
+        if (!ctx.chat || ctx.chat.type !== ChatType.Private)
+            return;
+
+        const user = ctx.message && ctx.message.from;
+        const userId = user && user.id;
+
+        if (!userId || !AuthSession[userId] || AuthSession[userId].status == UserStatus.Idle)
+            return;
+
+        let session = AuthSession[userId];
+        let msg = ctx.message && ctx.message.text;
+
+        switch (AuthSession[userId].status) {
+            case UserStatus.WaitingUsername:
+                session.data.email = msg + "";
+                session.status = UserStatus.WaitingPassword;
+
+                ctx.reply("接下来，请输入您的密码：\n若刚才的用户名输入错误，直接您发送的编辑那条消息即可");
+                break;
+            case UserStatus.WaitingPassword:
+                DoAuth(ctx, ctx.message!, session);
+                break;
         }
     });
 
-    function DoAuth(ctx: ContextMessageUpdate, ctxmsg: IncomingMessage, session) {
-        session.data.password = ctxmsg.text;
+    function DoAuth(ctx: ContextMessageUpdate, ctxmsg: IncomingMessage, session: IAuthSession) {
+        session.data.password = ctxmsg.text + "";
         ctx.reply("请稍等，正在进行认证");
+
         try {
             superagent
                 .post(cfg.api.url)
@@ -292,22 +333,26 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
                 .set('Useragent', 'MoeCraft Bot')
                 .end((err, res) => {
                     if (err) throw err;
-                    if (typeof (res) == "undefined" || res.text.length < 1)
+
+                    if (!res || res.text.length < 1)
                         throw new Error("接收认证服务器返回数据失败");
+
                     let data = JSON.parse(res.text);
-                    if (typeof (res) == "undefined" || !res)
+                    if (!data)
                         throw new Error("解析认证服务器返回数据失败");
+
                     if (data.status != 0) {
                         ctx.reply("错误：" + data.info);
                         return;
                     }
+
                     if (typeof (data.uid) != "undefined" && data.uid > 0) {
                         session.status = UserStatus.Done;
                         session.data.uid = data.uid;
                         session.data.name = data.name;
                         for (let key in Groups) {
-                            telegram.unbanChatMember(key, ctxmsg.from.id)
-                                .catch((error) => { });
+                            (telegram as any).unbanChatMember(key, ctxmsg.from!.id)
+                                .catch(() => { });
                         }
                         ctx.reply("认证成功: 欢迎回来，" + data.name + "\n感谢您加入 MoeCraft，以下是 MoeCraft Group 邀请链接：\n" + cfg.api.group + "\n本次会话结束后链接失效。为了确保安全，请在入群后删除本次会话");
                         return;
@@ -321,17 +366,16 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
         }
     }
 
-    fs.exists(dir + 'groups.json', (exists) => {
-        if (exists) {
-            fs.readFile(dir + 'groups.json', 'utf8', (err, data) => {
-                if (err) {
-                    console.warn("<!> Failed to load groups: groups.json");
-                    console.warn(err);
-                }
-                Groups = JSON.parse(data);
-            });
+    const exists = fse.existsSync(dir + 'groups.json');
+    if (exists) {
+        try {
+            const data = await fse.readFile(dir + 'groups.json', 'utf8');
+            Groups = JSON.parse(data);
+        } catch (err) {
+            console.warn("<!> Failed to load groups: groups.json");
+            console.warn(err);
         }
-    });
+    }
 
     if (Agent == null)
         console.log("Start polling telegram message (no proxy)");
@@ -339,4 +383,6 @@ console.log("MoeCraft Bot (Node) v1.0 Written by Kenvix");
         console.log("Start polling telegram message. Proxy type: " + cfg.tg.proxy.type);
 
     bot.startPolling();
-}) ();
+}
+
+main();
